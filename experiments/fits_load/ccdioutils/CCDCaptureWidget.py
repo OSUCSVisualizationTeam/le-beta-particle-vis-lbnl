c8151b128a802d7cd50ecd21cca429b7344bd66e
Juan V. Guerrero <coredumped@users.noreply.github.com>
2025-11-03 14:58:50 -0500

    Add colormap selection and view reset
    - Allow resetting view model
    - Add colormap selection combobox to CCDCaptureWidget toolbar

--- experiments/fits_load/ccdioutils/CCDCaptureWidget.py
+++ experiments/fits_load/ccdioutils/CCDCaptureWidget.py
@@ -1,5 +1,6 @@
 from PySide6 import QtWidgets, QtCore
 from .CCDCaptureViewModel import BaseCCDCaptureViewModel
+import matplotlib.pyplot as plt
 
 
 class CCDCaptureWidget(QtWidgets.QWidget):
@@ -13,6 +14,9 @@ class CCDCaptureWidget(QtWidgets.QWidget):
         self._toolbar = QtWidgets.QToolBar()
         self._vbox.addWidget(self._toolbar)
 
+        self._addColormapSelectionWidget()
+        self._addResetButton()
+
         self._visualizationWidget = QtWidgets.QLabel()
         self._visualizationWidget.setAlignment(QtCore.Qt.AlignCenter)
         self._vbox.addWidget(self._visualizationWidget)
@@ -20,8 +24,49 @@ class CCDCaptureWidget(QtWidgets.QWidget):
         self.setLayout(self._vbox)
         self._updateVisualization()
 
+    # Widget building
+
+    def _addColormapSelectionWidget(self):
+        colormapContainer = QtWidgets.QWidget()
+        colormapLayout = QtWidgets.QHBoxLayout(colormapContainer)
+        colormapLayout.setContentsMargins(0, 0, 0, 0)
+
+        colormapLabel = QtWidgets.QLabel("Colormap")
+        colormapLayout.addWidget(colormapLabel)
+
+        self._colormapComboBox = QtWidgets.QComboBox()
+        for cmap in plt.colormaps():
+            self._colormapComboBox.addItem(cmap)
+
+        default_colormap_index = self._colormapComboBox.findText("Greys_r")
+        if default_colormap_index != -1:
+            self._colormapComboBox.setCurrentIndex(default_colormap_index)
+
+        self._colormapComboBox.currentIndexChanged.connect(self._onColormapChanged)
+        colormapLayout.addWidget(self._colormapComboBox)
+        self._toolbar.addWidget(colormapContainer)
+
+    def _addResetButton(self):
+        resetButton = QtWidgets.QToolButton()
+        resetButton.setText("Reset")
+        resetButton.clicked.connect(self._resetViewModel)
+        self._toolbar.addWidget(resetButton)
+
+    # Callbacks
+
+    def _onColormapChanged(self, index: int):
+        """Callback invoked whenever the user selects a different colormap"""
+        selected_colormap = self._colormapComboBox.itemText(index)
+        self.__viewModel.setCurrentColormap(selected_colormap)
+        self._updateVisualization()
+
     def _updateVisualization(self):
         """Obtain data visualizable data from the view model"""
         display_data = self.__viewModel.getMatplotPixmap()
         if display_data:
             self._visualizationWidget.setPixmap(display_data)
+
+    def _resetViewModel(self):
+        """Resets view model state"""
+        self.__viewModel.reset()
+        self._updateVisualization()
